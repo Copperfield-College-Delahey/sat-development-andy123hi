62bfbcf913564b135cd958f0f9fa0f340a9052af
andy123hi <han0029@copperfieldcollege.vic.edu.au>
2025-06-25 12:45:19 +1000

    2nd lesson
    a start

--- gui.py
+++ gui.py
@@ -1,11 +1,17 @@
 import customtkinter as ctk
-from tkinter import messagebox, ttk
+from tkinter import messagebox
 
 app = ctk.CTk()
 app.title("waste log")
 app.geometry("1000x600")
 
-topFrame = ctk.CTkFrame(app, border_width=4)
-titleLabel = ctk.CTkLabel(topFrame, text="waste item", font=("Henlvetica", 30))
+titleLabel = ctk.CTkLabel(app, text="waste track", font=("Henlvetica", 30))
+titleLabel.pack(pady=20)
+
+nameEntry = ctk.CTkEntry(app, placeholder_text="name")
+nameEntry.pack(pady=10, padx=20)
+
+emailEntry = ctk.CTkEntry(app, placeholder_text="email")
+emailEntry.pack(pady=10, padx=20)
 
 app.mainloop()
