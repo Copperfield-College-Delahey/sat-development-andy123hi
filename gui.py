3fbb5b1a0f5a1fc9f27941dc3652d57001770d7a
andy123hi <han0029@copperfieldcollege.vic.edu.au>
2025-07-02 12:53:16 +1000

    5th lesson

--- gui.py
+++ gui.py
@@ -1,9 +1,27 @@
 import customtkinter as ctk
 from tkinter import messagebox
 
+
 app = ctk.CTk()
 app.title("waste log")
 app.geometry("1000x600")
+
+users = {}
+
+def signup():
+    name = nameEntry
+    email = emailEntry
+
+    if not name or not email:
+        messagebox.showerror("Error")
+        return
+
+    if email in users:
+
+
+def login():
+    name = nameEntry
+    email = emailEntry
 
 titleLabel = ctk.CTkLabel(app, text="waste track", font=("Henlvetica", 30))
 titleLabel.pack(pady=20)
--- log.py
+++ log.py
@@ -5,40 +5,9 @@ app = ctk.CTk()
 app.title("waste log")
 app.geometry("1000x600")
 
-title = ctk.CTkLabel(app, text="waste track", font=("Arial", 20))
+title = ctk.CTkLabel(app, text="waste logs", font=("Arial", 20))
 title.pack(pady=10)
 
-wasteItemLabel = ctk.CTkLabel(app, text="waste item")
-wasteItemLabel.pack(pady=10)
-
-wasteItemEntry = ctk.CTkEntry(app)
-wasteItemEntry.pack(pady=10)
-
-categoryLabel = ctk.CTkLabel(app, text="category")
-categoryLabel.pack(pady=10)
-
-categoryMenu = ctk.CTkComboBox(app, values=["recyclable", "reuseable", "disposable"])
-categoryMenu.pack(pady=10)
-
-dateLabel = ctk.CTkLabel(app, text="date")
-dateLabel.pack(pady=10)
-
-dateEntry = ctk.CTkEntry(app)
-dateEntry.pack(pady=10)
-
-idontknowButton = ctk.CTkButton(app, text="I don't know")
-idontknowButton.pack(pady=10)
-
-askforhelpButton = ctk.CTkButton(app, text="Ask for Help")
-askforhelpButton.pack(pady=10)
-
-feedbackButton = ctk.CTkButton(app, text="Feedback")
-feedbackButton.pack(pady=10)
-
-saveButton = ctk.CTkButton(app, text="Save")
-saveButton.pack(pady=10)
-
-viewpastEntry = ctk.CTkButton(app, text="View past Entry")
-viewpastEntry.pack(pady=10)
+pastentry
 
 app.mainloop()
